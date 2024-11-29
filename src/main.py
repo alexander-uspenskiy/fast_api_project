b91b02fc794b96281999bc82aa84e6522ba7900e
Alex <alexander.uspenskiy@gmail.com>
2024-11-29 16:01:13 -0500

    add basic routes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse  # Import FileResponse
+from fastapi.staticfiles import StaticFiles
+app = FastAPI()
+
+@app.get("/hello")
+def hello(name:str):
+    return {"Hello": f"How you doing, {name}?"}
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my FastAPI app!"}
+@app.get("/favicon.ico")
+def get_favicon():
+    return FileResponse("static/favicon.ico")
